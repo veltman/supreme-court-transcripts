bf5d95335ab483ae62d3b062841e58fe26a03606
Noah <noah@noahveltman.com>
2015-04-17 21:48:05 -0400

    Making this less bad

--- clean.js
+++ clean.js
@@ -1,63 +1,60 @@
 var fs = require("fs"),
-    queue = require("queue-async"),
     _ = require("underscore"),
-    cases = require("./cases.json");
+    cases = shuffle(require("./cases.json")); // Randomize for better spot checking
 
-cases = shuffle(cases);
+// Get each raw case text and structure it
+cases = cases.forEach(function(details){
 
-cases = cases.forEach(function(c){
+  var raw = require("./json/" + details.caseNumber + ".json");
 
-  var raw = require("./json/" + c.caseNumber + ".json");
-
-  fs.writeFileSync("txt/" + c.caseNumber + ".txt",clean(raw,c).join("\n"));
+  // Write the final version out to JSON
+  fs.writeFileSync("final/" + details.caseNumber + ".json",JSON.stringify(clean(raw,details)));
 
 });
 
+// Clean everything up
 function clean(pages,details) {
 
-  console.log(details.filename);
-
-  // remove index pages at the end
+  // Look for the first index page, lop off everything after it
   for (var i = 0; i < pages.length; i++) {
-    if (numColons(pages[i]) > 50) {
+    if (isIndexPage(pages[i])) {
       pages = pages.slice(0,i);
       break;
     }
   }
 
-  var coverPageFound,
-      contentsPageFound;
-
-  // remove cover pages
+  // Look for the last cover page, lop off everything before it
   for (var i = Math.min(2,pages.length-1); i >= 0; i--) {
     if (isCoverPage(pages[i])) {
       pages = pages.slice(i+1);
-      coverPageFound = true;
       break;
     }
   }
 
-  // remove table of contents
+  // Look for the first table of contents page, lop off everything before it
   for (var i = Math.min(3,pages.length-1); i >= 0; i--) {
     if (isContentsPage(pages[i])) {
       pages = pages.slice(i+1);
-      contentsPageFound = true;
       break;
     }
   }
 
-  // remove certification pages
+  // Remove certification pages
   pages = pages.filter(function(page,i){
 
     return !isCertificationPage(page);
 
   });
 
-  // Remove the proceedings header and stray page numbers and timestamps and "Official"
+  // Remove some junk:
+  // "PROCEEDINGS" header
+  // Stray page numbers
+  // Timestamp at the beginning of the transcript
+  // "OFFICIAL" header
   pages = pages.map(function(page,i){
 
     page = page.filter(function(line){
-      if (line.match(/^\s*[0-9]*\s*P\s*R\s*O\s*C\s*E\s*E\s*D\s*I\s*N\s*G\s*S\s*$/)) {
+      if (line.match(/^\s*[0-9]*\s*P\s*R\s*O\s*C\s*E\s*E\s*D\s*I\s*N\s*G\s*S\s*[.]?$/)) {
         return false;
       }
 
@@ -68,14 +65,12 @@ function clean(pages,details) {
       return true;
     });
 
-    //
     if (page[0].match(/^\s*Official\s*$/)) {
       page.shift();
     } else if (page[0].match(/^\s*Official/) && page[0].match(/subject/i) && page[0].match(/review/i)) {
       page.shift();
     }
 
-    // Remove timestamp
     if (i === 0 && page[0].match(/[0-9]+[:][0-9]{2}\s*(a|p)[.]?m[.]?/)) {
       page.shift();
     }
@@ -99,12 +94,89 @@ function clean(pages,details) {
     return line.replace(/\s/g,"").length;
   });
 
-  return flattened;
+  return getSpeakers(flattened);
 
 }
 
+// Turn a list of cleaned lines into an array of lines of speech by speaker
+function getSpeakers(flattened) {
+
+  var lines = [],
+      currentSpeaker = null,
+      buffer = [];
+
+  for (var i = 0; i < flattened.length; i++) {
+
+    var speaker = getSpeaker(flattened[i]),
+        isLaughter = flattened[i].match(/[(]laughter[.][)]/i);
+
+    // Flush the buffer to lines
+    if (isLaughter || speaker) {
+
+      if (buffer.length) {
+
+        lines.push({
+          speaker: currentSpeaker,
+          text: buffer.join(" ").replace(/\s+/g," ")
+        });
+
+      }
+
+      currentSpeaker = speaker || null;
+      buffer = [];
+
+    }
+
+    // If it's a REBUTTAL ARGUMENT, ORAL ARGUMENT header we don't want it
+    if (!speaker && flattened[i].match(/^\s+[A-Z]+[^a-z]+[A-Z,]\s*$/)) {
+
+      continue;
+
+    } else if (speaker) {
+
+      // Add text minus speaker
+      buffer.push(flattened[i].split(":").slice(1).join(":").trim());
+
+    } else {
+
+      //Add all text
+      buffer.push(flattened[i].trim());
+
+    }
+
+  }
+
+  // Flush anything left in the buffer to lines
+  if (buffer.length) {
+    lines.push({
+      speaker: currentSpeaker,
+      text: buffer.join(" ").replace(/\s+/g," ")
+    });
+  }
+
+  return lines;
+
+}
+
+// Get the speaker of the line
+// (multiple all caps words followed by a colon to start the line)
+function getSpeaker(line) {
+
+  var match = line.match(/^(\s+[A-Z][A-Z-]+[.]?){1,4}[:]/);
+
+  if (match) {
+    return match[0].replace(":","").replace(/\s+/g," ").replace("CHIEF JUSTICE","JUSTICE").trim();
+  }
+
+  return null;
+
+}
+
+// Remove the Alderson Reporting Company's footer
 function removeAlderson(lines) {
 
+  // Look for a line about Alderson Reporting Company
+  // lop it off and everything after it
   for (var i = 0; i < lines.length; i++) {
     if (lines[i].match(/alderson\s+reporting/i)) {
       return lines.slice(0,i);
@@ -115,6 +187,7 @@ function removeAlderson(lines) {
 
 }
 
+// Remove line numbers, these are pretty erratic
 function removeLineNumbers(lines) {
 
   return lines.map(function(line){
@@ -125,6 +198,7 @@ function removeLineNumbers(lines) {
 
 }
 
+// Remove the final parenthetical about the case being submitted
 function removeSubmitted(lines) {
   for (var i = lines.length-1; i >= 0; i--) {
     if (lines[i].match(/[(]whereupon/i)) {
@@ -135,37 +209,39 @@ function removeSubmitted(lines) {
   console.log(lines);
 }
 
+// It's a certification page if the first line says "certification" and the last line says "reporter"
 function isCertificationPage(page) {
 
-  var isCertification = page[0].trim().match(/certification/i) && page[page.length-1].trim().match(/reporter/i);
-
-  return isCertification;
+  return page[0].trim().match(/certification/i) && page[page.length-1].trim().match(/reporter/i);
 
 }
 
+// Is it a table of contents page?
 function isContentsPage(page) {
 
-  var contents = !!page.slice(0,5).filter(function(line){
+  // Does it have a line with just the word "CONTENTS" on it near the top?
+  return !!page.slice(0,5).filter(function(line){
     return line.replace(/[^a-z]/ig,"") === "CONTENTS";
   }).length;
 
-  if (contents) {
-    return true;
-  }
-
-  return false;
-
 }
 
+// Is it a cover page?
 function isCoverPage(page) {
 
+  // Does it have an "IN THE SUPREME COURT OF THE UNITED STATES" header
+  // give or take punctuation and spacing
   var header = !!page.filter(function(line){
     return line.replace(/[^a-z]/ig,"") === "INTHESUPREMECOURTOFTHEUNITEDSTATES";
   }).length;
 
+  // Does it have "v." somewhere
   var v = page.join("").match(/\sv[.]\s/);
+
+  // Does it have the case number abbreviation "no." somewhere
   var no = page.join("").match(/no[.]\s/i);
 
+  // If those three are true, consider it a cover page
   if (header && v && no) {
     return true;
   }
@@ -174,12 +250,14 @@ function isCoverPage(page) {
 
 }
 
-function numColons(page) {
+// It's an index page if it has a crapload of colons in it
+function isIndexPage(page) {
 
-  return (page.join("").match(/[:]/g) || []).length;
+  return (page.join("").match(/[:]/g) || []).length > 50;
 
 }
 
+// Fisher-Yates via Mike Bostock
 function shuffle(array) {
   var m = array.length, t, i;
 
